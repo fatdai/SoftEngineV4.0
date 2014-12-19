9ff6ba308a1bebb1876bdf7897b6426d74211d72
daihanlong <daihanlongc2dm@gmail.com>
2014-12-19 21:33:39 +0800

    Initial Commit

---
+++ SoftEngineV4.0/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SoftEngineV4.0
+//
+//  Created by user on 14-12-19.
+//  Copyright (c) 2014年 user. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
